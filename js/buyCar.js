49f3c1d5bbfdc36bcf09fe6c03e4cfa4af854daf
esteban salas <esalassulca@gmail.com>
2022-07-27 22:07:52 -0500

    Agrege tabla de compras

--- js/buyCar.js
+++ js/buyCar.js
@@ -1,42 +1,80 @@
 const d=document
 export default function buyCar(){
-
+  console.log("hola")
+  const $table=d.querySelector("table")
+  const $tbody=d.querySelector("table tbody")
+  const $total=d.querySelector("tfoot td[data-total]");
   const products=[]
+  const $enlaces=d.querySelectorAll(".item a");
+  $enlaces.forEach(el=>{
+    el.addEventListener("click",e=>{
+      e.preventDefault();
+    })
+  })
 
   d.querySelectorAll(".item").forEach((el)=>{
-    
-     const a=el.addEventListener("click",(e)=>{
+
+     el.addEventListener("click",(e)=>{
+      
       const $id=el.dataset.id;
       const $name= el.querySelector("span[data-name]").textContent
-      const $buy=el.querySelector("p[data-buy]").textContent
+      const $buy=Number(el.querySelector("span[data-buy]").textContent)
       
       const product={
         id:$id,
         name:$name,
         buy:$buy,
-        total:$buy
+        total:$buy,
+        cantidad:1
       }
       
-    })
+      const isProduct=products.find(el=> el.id===product.id);
+
+      if(!isProduct){
+        products.push(product)
+        console.log(product)
+      }else{
+        isProduct.total+=isProduct.buy
+        isProduct.cantidad+=1;
+        
+      }
+      getIntoDate(products)
+      let total=0;
+      products.forEach(el=>total+=el.total)
+      $total.textContent=total
+
+
+    })  
+
+    
+    
     
   })
-  
-
-    function validar(products){
-    products.map(el=> el.$id===product.id ?el.total+el.buy : products.push(product))
-      
-    }  
 
 
-  function getIntoDate(cantidad,producto,precio,total){
-    console.log(items[0])
-    const $tbody=d.querySelector("table tbody")
-    const $template=d.querySelector("template").content
-    const $tds=$template.querySelectorAll("td")
-    $tds[0].textContent=0
-    $tds[1].textContent=0
-    $tds[2].textContent=0
-    $tds[3].textContent=0
+  function getIntoDate(el){
+    const $fragment=d.createDocumentFragment();
+    
+    $table.classList.remove("hidden")
+    console.log($tbody)
+    $tbody.textContent="";
+    function plantilla({name,buy,total,cantidad}){
+      const $template=d.querySelector("template").content
+      const $tds=$template.querySelectorAll("td")
+      $tds[0].textContent=cantidad;
+      $tds[1].textContent=name
+      $tds[2].textContent=buy
+      $tds[3].textContent=total
+      const $node= $template.cloneNode(true);
+      $fragment.appendChild($node)
+    }
+   
+    el.forEach(el=>{
+      plantilla(el)
+
+    })
+    $tbody.appendChild($fragment)
+ 
   }
   
 }
